b13820f9197c53df6796d34274ecbc71e6c67aa2
Anderson Lugo <andersonlugojacome@hotmail.com>
2023-12-12 13:38:19 -0500

    updated

--- my-proyect/src/App.jsx
+++ my-proyect/src/App.jsx
@@ -2,7 +2,7 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
- import MiBoton from './components/MiBoton'
+//  import MiBoton from './components/MiBoton'
 
  import ContadorClass from './components/ContadorClass'
 
